b739dfebc271f9e485eabd6a51848d87b7d7ddd9
Kaylaesmith1 <kaylaesmith1@gmail.com>
2021-10-02 17:42:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,4 +9,3 @@ function App() {
   );
 }
 
-export default App;
