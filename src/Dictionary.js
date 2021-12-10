9a357b076fb1002a75ffda8efad13c6c71ba895a
Kaylaesmith1 <kaylaesmith1@gmail.com>
2021-12-10 17:16:22 +0100

    added documentation and results section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -18,6 +18,7 @@ function search(event){
 event.preventDefault();
 }
 
+//documentation: https://dictionaryapi.dev/
 let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 
@@ -31,7 +32,7 @@ function handleKeywordChange(event) {
       <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
 
     </form>
-    <Results />
+    <Results results={results}/>
     
     </div>
   );
