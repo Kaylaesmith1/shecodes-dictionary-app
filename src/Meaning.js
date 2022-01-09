225ba104ec576e3337206a7577ff4e110a3ff84f
Kaylaesmith1 <kaylaesmith1@gmail.com>
2022-01-09 22:45:11 +0100

    Synonym styling
    Made li a horizontal list

--- src/Meaning.js
+++ src/Meaning.js
@@ -12,13 +12,13 @@ export default function Meaning(props) {
     {props.meaning.definitions.map(function(definitions, index){
       return (
         <div key={index}>
-          <p>
+          
           <strong>Definition: </strong>{definitions.definition}
           <br />
           <strong>Example: </strong><em>{definitions.example}</em>
           <br />
 <Synonyms synonyms={definitions.synonyms}/>
-</p>
+
     </div>
       );
       
