7b6534fb4e1da87585c01f4da25c6831417b7c75
Kaylaesmith1 <kaylaesmith1@gmail.com>
2022-01-09 13:21:55 +0100

    Update Meaning.js
    Store API results (definitions and examples)

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+
+//to get the FIRST meaning of a word: console.log(response.data[0].meanings[0].definitions[0].definition); To loop through all defs use 'map'
+//em used to make italics
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
-    {props.meaning.definitions.map(function(definition, index){
+    {props.meaning.definitions.map(function(definitions, index){
       return (
         <div key={index}>
           <p>
-          {definition.definition}
+          {definitions.definition}
           <br />
-          <em>{definition.example}</em>
+          <em>{definitions.example}</em>
+          <br />
           </p>
     </div>
       );
