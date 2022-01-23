48b593959b0f578731321f3e1c0fd6efc24e3ffe
Kaylaesmith1 <kaylaesmith1@gmail.com>
2022-01-23 11:20:53 +0100

    Phonetics
    Added Phonetics to return the pronunciation and phonetic spelling of the searched word.

---
+++ src/Phonetics.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  
+  return (
+    <div className="Phonetics">
+      <a className="audio" href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      Listen
+      </a>
+      
+      <br />
+        {props.phonetic.text}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  console.log(props.definition);
 if (props.definition) {
   return (
   <div className="Results">
     <h2>{props.definition.word}</h2>
+
+    {props.definition.phonetics.map(function(phonetic, index) {
+      return (
+        <div key={index}>
+          <Phonetics phonetic={phonetic} />
+          </div>
+      )
+    })}
+
     {props.definition.meanings.map(function 
     (meaning, index) {
       return (
