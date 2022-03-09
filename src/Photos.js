3af3d4a81beacc075dec1c5aac86f1e9fea58733
Kaylaesmith1 <kaylaesmith1@gmail.com>
2022-03-09 20:16:32 +0100

    Must have alt prop
    Added alt prop to img src

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
       return (
       <div className="col-4" key={index}>
         <a href={photo.src.large} target="_blank" rel="noreferrer"> {copyright}
-        <img src={photo.src.tiny} className="image-fluid"/>
+        <img src={photo.src.tiny} className="image-fluid" alt=""/>
         </a>
         </div>
       );
